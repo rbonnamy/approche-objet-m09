3f65851b9fbe75fcd1a0e570dfdd9a842015cee3
rbonnamy <rbonnamy@diginamic.fr>
2023-11-02 15:20:15 +0100

    TP2: fin

--- entites/TestPersonne.java
+++ entites/TestPersonne.java
@@ -5,7 +5,9 @@ import entites2.Personne;
 public class TestPersonne {
     public static void main(String[] args) {
         AdressePostale adr = new AdressePostale(50, "place du Marché", 44100, "Nantes");
+        Personne p1 = new Personne("LEE", "Arg", adr);
 
-        Personne p = new Personne("LEE", "Arg", adr);
+        Personne p2 = new Personne("KIOP", "Blah");
+        p2.adresse = adr;
     }
 }
--- entites2/Personne.java
+++ entites2/Personne.java
@@ -7,9 +7,15 @@ public class Personne {
     public String prenom;
     public AdressePostale adresse;
 
+    public Personne(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
     public Personne(String nom, String prenom, AdressePostale adresse) {
         this.nom = nom;
         this.prenom = prenom;
         this.adresse = adresse;
     }
+
 }
